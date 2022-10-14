854f5968f833f275d844ce2f39af3ee24461c8e5
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2022-10-14 18:41:24 +0300

    fix bag modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -35,7 +35,7 @@ export function App() {
           return;
         }
 
-        setImages(state => [...state, ...images]);
+        setImages(prevImages => [...prevImages, ...images]);
         setCurrentImgPerPage(images.length);
       } catch {
         toast.error('Failed to load images :(');
@@ -69,7 +69,7 @@ export function App() {
   };
 
   const loadMoreBtn = () => {
-    setCurrentPage(state => state + 1);
+    setCurrentPage(сurrentPage => сurrentPage + 1);
   };
 
   return (
@@ -113,144 +113,3 @@ export function App() {
     </div>
   );
 }
-
-// export class OldApp extends Component {
-//   state = {
-//     images: [],
-//     searchQuery: '',
-//     currentPage: 1,
-//     isLoading: false,
-//     showModal: false,
-//     largeImageURL: '',
-//     tags: '',
-//     currentImgPerPage: null,
-//   };
-
-//   async componentDidUpdate(_, prevState) {
-//     const prevQuery = prevState.searchQuery;
-//     const nextQuery = this.state.searchQuery;
-//     const prevCurrentPage = prevState.currentPage;
-//     const nextCurrentPage = this.state.currentPage;
-
-//     if (nextQuery === '') {
-//       toast.error(`Please enter text to search`);
-//       return;
-//     }
-
-//     if (prevQuery !== nextQuery || prevCurrentPage !== nextCurrentPage) {
-//       try {
-//         this.setState({ isLoading: true });
-
-//         const images = await getImages(nextQuery, this.state.currentPage);
-
-//         if (images.length === 0) {
-//           this.setState({ currentImgPerPage: null });
-//           toast.error(`Images ${this.state.searchQuery} not found`);
-//           return;
-//         }
-
-//         this.setState(prevState => ({
-//           images: [...prevState.images, ...images],
-//           currentImgPerPage: images.length,
-//         }));
-//       } catch {
-//         toast.error('Failed to load images :(');
-//         // this.setState({ error: 'Failed to load images :(' });
-//       } finally {
-//         this.setState({ isLoading: false });
-//       }
-//     }
-
-//     if (this.state.currentPage > 1) {
-//       window.scrollBy({
-//         top: 400,
-//         behavior: 'smooth',
-//       });
-//     }
-//   }
-
-//   searchImages = searchText => {
-//     if (this.state.searchQuery === searchText) {
-//       return;
-//     }
-
-//     this.setState(() => ({
-//       images: [],
-//       currentPage: 1,
-//       searchQuery: searchText,
-//     }));
-//   };
-
-//   toggleModal = () => {
-//     this.setState(({ showModal }) => ({
-//       showModal: !showModal,
-//     }));
-//   };
-
-//   openModal = (largeImageURL, tags) => {
-//     this.toggleModal();
-
-//     this.setState(() => ({
-//       largeImageURL,
-//       tags,
-//     }));
-//   };
-
-//   loadMore = () => {
-//     this.setState(prevState => ({
-//       currentPage: prevState.currentPage + 1,
-//     }));
-//   };
-
-//   render() {
-//     const {
-//       isLoading,
-//       images,
-//       largeImageURL,
-//       tags,
-//       showModal,
-//       currentImgPerPage,
-//     } = this.state;
-
-//     return (
-//       <div className={css.app}>
-//         <Searchbar onSubmit={this.searchImages} isSubmitting={isLoading} />
-
-//         {images.length > 0 && (
-//           <>
-//             <ImageGallery images={images} onClickImg={this.openModal} />
-//             {currentImgPerPage < PER_PAGE && (
-//               <h2 className={css.no__more}>No more pictures :(</h2>
-//             )}
-//           </>
-//         )}
-
-//         {showModal && (
-//           <Modal
-//             largeImageURL={largeImageURL}
-//             tags={tags}
-//             onClose={this.toggleModal}
-//           />
-//         )}
-
-//         <Loader isLoading={isLoading} />
-
-//         {currentImgPerPage === PER_PAGE && (
-//           <LoadMoreBtn onLoadMore={this.loadMore} />
-//         )}
-
-//         <Toaster
-//           position="top-right"
-//           reverseOrder={false}
-//           toastOptions={{
-//             duration: 5000,
-//             style: {
-//               background: '#ff0000',
-//               color: '#fff',
-//             },
-//           }}
-//         />
-//       </div>
-//     );
-//   }
-// }
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,43 +1,38 @@
 import PropTypes from 'prop-types';
 import css from '../../styles/Styles.module.css';
 import { createPortal } from 'react-dom';
-import { Component } from 'react';
+import { useEffect } from 'react';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+export function Modal({ largeImageURL, tags, onClose }) {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  });
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = event => {
+  const handleKeyDown = event => {
     if (event.code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  handleBackdropClick = event => {
+  const handleBackdropClick = event => {
     if (event.currentTarget === event.target) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    const { largeImageURL, tags } = this.props;
-
-    return createPortal(
-      <div className={css.overlay} onClick={this.handleBackdropClick}>
-        <div className={css.modal}>
-          <img src={largeImageURL} alt={tags} />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
+  return createPortal(
+    <div className={css.overlay} onClick={handleBackdropClick}>
+      <div className={css.modal}>
+        <img src={largeImageURL} alt={tags} />
+      </div>
+    </div>,
+    modalRoot
+  );
 }
 
 Modal.propTypes = {
